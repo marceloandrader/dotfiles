3efeaa3d2dd0718b95f7781d5f5957ca58341854
Marcelo Andrade R. <marcelo.andrade.r@gmail.com>
2024-11-19 09:21:22 -0500

    fish env

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,7 @@ set -gx EDITOR nvim
 set -gx TERM screen-256color
 starship init fish | source
 complete -c remote -a "(fdfind  . ~/.password-store/ | sed 's/\/home\/marcelo\/\.password-store\///g' | sed 's/\.gpg//g')"
-source ~/.asdf/asdf.fish
+# source ~/.asdf/asdf.fish
 eval (dircolors ~/.dir_colors/dircolors | head -n 1 | sed 's/^LS_COLORS=/set -x LS_COLORS /;s/;$//')
 export PATH="/home/marcelo/.local/share/solana/install/active_release/bin:$PATH"
 export PATH="/usr/local/go/bin:$PATH"
@@ -24,3 +24,4 @@ export PATH="/home/marcelo/.gem/bin:$PATH"
 # tabtab source for electron-forge package
 # uninstall by removing these lines or running `tabtab uninstall electron-forge`
 [ -f /home/marcelo/Code/toptal/SmithCarson/sc-source/scrape-bot/node_modules/tabtab/.completions/electron-forge.fish ]; and . /home/marcelo/Code/toptal/SmithCarson/sc-source/scrape-bot/node_modules/tabtab/.completions/electron-forge.fish
+mise activate fish | source
